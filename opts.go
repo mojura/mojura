576d13429841a4ac790393f7fc7cb066883789f8
Josh Montoya <josh@montoya.io>
2020-03-13 12:08:08 -0700

    Adjust defaults

--- opts.go
+++ opts.go
@@ -4,9 +4,9 @@ import "time"
 
 const (
 	// DefaultMaxBatchCalls is the default maximum number of calls a batch will take
-	DefaultMaxBatchCalls = 64
+	DefaultMaxBatchCalls = 1024
 	// DefaultMaxBatchDuration is the default maximum duration a batch will take to collect calls
-	DefaultMaxBatchDuration = time.Millisecond * 400
+	DefaultMaxBatchDuration = time.Millisecond * 10
 	// DefaultRetryBatchFail is the default value for if a batch call will retry when a batch sibling fails
 	DefaultRetryBatchFail = true
 )
