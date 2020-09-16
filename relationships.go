3d4d1f2a7ef0a0c709185d7ef84e02d9b132b938
Josh Montoya <josh@montoya.io>
2020-09-16 09:26:14 -0700

    implement relationships

---
+++ relationships.go
@@ -0,0 +1,67 @@
+package dbl
+
+// Relationships help to store the relationships for an Entry
+type Relationships []Relationship
+
+// Append will append a set of relationship IDs for a given relationship
+func (r *Relationships) Append(relationshipIDs ...string) {
+	*r = append(*r, relationshipIDs)
+}
+
+// Relationship help to store the IDs for an Entry's relationship
+type Relationship []string
+
+// Has will note if a relationship has a particular relationship ID
+func (r Relationship) Has(relationshipID string) (has bool) {
+	for _, id := range r {
+		if id == relationshipID {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Delta will call onAdd and onRemove for the delta entries between the current and old Relationship
+func (r Relationship) Delta(old Relationship, onAdd, onRemove RelationshipFn) (err error) {
+	if err = r.addNew(old, onAdd); err != nil {
+		return
+	}
+
+	if err = r.removeOld(old, onRemove); err != nil {
+		return
+	}
+
+	return
+}
+
+func (r Relationship) addNew(old Relationship, onAdd RelationshipFn) (err error) {
+	for _, id := range r {
+		if old.Has(id) {
+			continue
+		}
+
+		if err = onAdd([]byte(id)); err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func (r Relationship) removeOld(old Relationship, onRemove RelationshipFn) (err error) {
+	for _, id := range old {
+		if r.Has(id) {
+			continue
+		}
+
+		if err = onRemove([]byte(id)); err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+// RelationshipFn is called for relationship comparison funcs
+type RelationshipFn func(relationshipID []byte) error
