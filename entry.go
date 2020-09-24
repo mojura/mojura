327a9e878e733bc0ac781dba90f79e9e09970ee7
Josh Montoya <josh@montoya.io>
2020-09-24 12:44:18 -0700

    Add deprecated tag

--- entry.go
+++ entry.go
@@ -28,7 +28,7 @@ func (e *Entry) GetUpdatedAt() (updatedAt int64) {
 }
 
 // GetRelationshipIDs will get the associated relationship IDs
-// Note: This method is now deprecated. The method has been kept and the signature
+// Deprecated: This method is now deprecated. The method has been kept and the signature
 // has been changed to ensure previous use of this method would be easily caught by
 // the compiler. If this method was removed, the fear is that the prior use of the
 // unused method would still continue (since the interface would still technically match).
