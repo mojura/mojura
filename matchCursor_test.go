38659e7f7965a1c52105aced746d24674b0839bb
Josh Montoya <josh@montoya.io>
2021-01-14 12:56:55 -0800

    implement HasForward and HasReverse tests

--- matchCursor_test.go
+++ matchCursor_test.go
@@ -543,3 +543,112 @@ func Test_matchCursor_Last(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func Test_matchCursor_HasForward(t *testing.T) {
+	fn := func(c *matchCursor, entryID []byte) (value bool, err error) {
+		return c.HasForward(entryID)
+	}
+
+	testMatchCursorHas(t, fn)
+}
+
+func Test_matchCursor_HasReverse(t *testing.T) {
+	fn := func(c *matchCursor, entryID []byte) (value bool, err error) {
+		return c.HasReverse(entryID)
+	}
+
+	testMatchCursorHas(t, fn)
+}
+
+func testMatchCursorHas(t *testing.T, fn func(c *matchCursor, entryID []byte) (value bool, err error)) {
+	type expected struct {
+		value bool
+		err   error
+	}
+
+	type testcase struct {
+		relationshipKey string
+		relationshipID  string
+		entryID         string
+		expected        expected
+	}
+
+	tcs := []testcase{
+		{
+			relationshipKey: "users",
+			relationshipID:  "user_1",
+			entryID:         "00000001",
+			expected:        expected{value: true},
+		},
+		{
+			relationshipKey: "contacts",
+			relationshipID:  "contact_2",
+			entryID:         "00000000",
+			expected:        expected{value: false},
+		},
+		{
+			relationshipKey: "groups",
+			relationshipID:  "group_1",
+			entryID:         "00000002",
+			expected:        expected{value: true},
+		},
+	}
+
+	testMatchCursor(t, func(txn *Transaction) (err error) {
+		for i, tc := range tcs {
+			var cur *matchCursor
+			if cur, err = newMatchCursor(txn, []byte(tc.relationshipKey), []byte(tc.relationshipID)); err != nil {
+				return
+			}
+
+			exp := tc.expected
+
+			var ok bool
+			if ok, err = fn(cur, []byte(tc.entryID)); err != exp.err {
+				err = fmt.Errorf("invalid error, expected <%v> and received <%v> (test case #%d)", exp.err, err, i)
+				return
+			}
+
+			if ok != exp.value {
+				err = fmt.Errorf("invalid value, expected <%v> and received <%v> (test case #%d)", exp.value, ok, i)
+				return
+			}
+		}
+
+		return
+	})
+}
+
+func testMatchCursor(t *testing.T, fn func(*Transaction) error) {
+	var (
+		m   *Mojura
+		err error
+	)
+
+	if m, err = testInit(); err != nil {
+		t.Fatal(err)
+	}
+	defer testTeardown(m)
+
+	a := newTestStruct("user_0", "contact_0", "group_3", "1")
+	b := newTestStruct("user_1", "contact_2", "group_2", "2")
+	c := newTestStruct("user_2", "contact_2", "group_1", "3")
+
+	if err = m.Transaction(context.Background(), func(txn *Transaction) (err error) {
+		if _, err = txn.New(a); err != nil {
+			return
+		}
+
+		if _, err = txn.New(b); err != nil {
+			return
+		}
+
+		if _, err = txn.New(c); err != nil {
+			return
+		}
+
+		return fn(txn)
+	}); err != nil {
+		t.Fatal(err)
+	}
+}
