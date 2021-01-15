1e40f2c1794a987d68ca02d74aaa4de59b9ff832
Josh Montoya <josh@montoya.io>
2021-01-15 14:20:26 -0800

    implement NewIteratingOpts

--- iteratingOpts.go
+++ iteratingOpts.go
@@ -1,5 +1,14 @@
 package mojura
 
+var defaultIteratingOpts = &IteratingOpts{}
+
+// NewIteratingOpts will initialize a new instance of Iterating Opts
+func NewIteratingOpts(fs ...Filter) *IteratingOpts {
+	var i IteratingOpts
+	i.Filters = fs
+	return &i
+}
+
 // IteratingOpts represents iterating options
 type IteratingOpts struct {
 	SeekTo  string
