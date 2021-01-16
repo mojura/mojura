be296abea2d73dcd053340e1d33d8b5bad61b8c1
Josh Montoya <josh@montoya.io>
2021-01-15 16:10:27 -0800

    remove non needed methods

--- iteratingOpts.go
+++ iteratingOpts.go
@@ -11,31 +11,7 @@ func NewIteratingOpts(fs ...Filter) *IteratingOpts {
 
 // IteratingOpts represents iterating options
 type IteratingOpts struct {
-	SeekTo  string
+	LastID  string
 	Reverse bool
 	Filters []Filter
-}
-
-func (i *IteratingOpts) isReverse() bool {
-	if i == nil {
-		return false
-	}
-
-	return i.Reverse
-}
-
-func (i *IteratingOpts) seekTo() string {
-	if i == nil {
-		return ""
-	}
-
-	return i.SeekTo
-}
-
-func (i *IteratingOpts) filters() []Filter {
-	if i == nil {
-		return nil
-	}
-
-	return i.Filters
 }
