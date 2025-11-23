d2212cd3c6958afb15785c82d3d0d6fe881847f0
Josh Montoya <josh@montoya.io>
2025-11-23 08:43:14 -0800

    move JSONEncoder to it's own file

--- encoder.go
+++ encoder.go
@@ -1,22 +1,7 @@
 package mojura
 
-import "encoding/json"
-
 // Encoder represents an encoder for Mojura Entries
 type Encoder interface {
-	Marshal(interface{}) ([]byte, error)
-	Unmarshal([]byte, interface{}) error
-}
-
-// JSONEncoder represents a JSON encoder
-type JSONEncoder struct{}
-
-// Marshal is an encoding helper method
-func (j *JSONEncoder) Marshal(value interface{}) (bs []byte, err error) {
-	return json.Marshal(value)
-}
-
-// Unmarshal is a decoding helper method
-func (j *JSONEncoder) Unmarshal(bs []byte, val interface{}) (err error) {
-	return json.Unmarshal(bs, val)
+	Marshal(any) ([]byte, error)
+	Unmarshal([]byte, any) error
 }
